2f3c2595b08ed198400d23422d53f0b7b720b866
Daniil Smoliakov <danonofficial@gmail.com>
2021-03-17 12:41:32 +0000

    pre render cleanup

--- shaders/preRender.fragment.glsl
+++ shaders/preRender.fragment.glsl
@@ -1,16 +1,15 @@
-#version 330 core
+#version 420 core
 layout(location = 0) out vec4 normalColor;
 layout(location = 1) out vec4 depthColor;
 layout(location = 2) out vec4 metallicColor;
+
 in vec4 pos;
 in vec4 normal;
 in vec2 texCoord;
+
 uniform sampler2D textureMetallic;
-// material parameters
 
-
-void main()
-{
+void main() {
     normalColor = normal;
     depthColor = vec4(vec3(gl_FragCoord.z), 1.f);
     float metallic = texture(textureMetallic, texCoord).r;
--- shaders/preRender.vertex.glsl
+++ shaders/preRender.vertex.glsl
@@ -1,4 +1,4 @@
-#version 330
+#version 420 core
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normals;
 layout (location = 2) in vec2 textures;
@@ -6,15 +6,15 @@ layout (location = 2) in vec2 textures;
 out vec4 pos;
 out vec4 normal;
 out vec2 texCoord;
+
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
-    gl_Position = projection * view * model * vec4(position, 1.0f);
-    pos = view * model * vec4(position, 1.0f);
-    normal = view * model * vec4(normals, 1.0f);
+void main() {
+    mat4 mv = view * model;
+    gl_Position = projection * mv * vec4(position, 1.0f);
+    pos = mv * vec4(position, 1.0f);
+    normal = mv * vec4(normals, 1.0f);
     texCoord = textures;
 }
-//
